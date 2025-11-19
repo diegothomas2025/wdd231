340c272f832213f352db2bac0485a08a4c184d7a
¨diegothomas2025¨ <diegothomas4@hotmail.com>
2025-11-18 22:33:10 -0300

    Learning activity week03

---
+++ week03/scripts/weather.js
@@ -0,0 +1,5 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.63&appid=51acc09c401af808d7a903d7e8d2fe35';
