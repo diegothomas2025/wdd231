f6bc49842ff5bc48d35a9bbc2f5afc23b9f6d7a2
diegothomas2025 <diegothomas4@hotmail.com>
2025-11-19 10:47:36 -0300

    Consuming an API Activity Completed

--- week03/scripts/weather.js
+++ week03/scripts/weather.js
@@ -2,4 +2,29 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.63&appid=51acc09c401af808d7a903d7e8d2fe35';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.63&units=metric&appid=51acc09c401af808d7a903d7e8d2fe35';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        } 
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;C`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+};
+
+apiFetch();
