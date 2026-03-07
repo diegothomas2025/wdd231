824a8ea865082e391b7c06c48bb51e4997a435cb
diegothomas2025 <diegothomas4@hotmail.com>
2026-03-06 21:22:55 -0300

    learning activities completed

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,5 +1,7 @@
-const navButton = document.querySelector("#ham-button");
+const navButton = document.querySelector('#ham-button');
+const navLink = document.querySelector('#nav-bar');
 
-navButton.addEventListener("click", () => {
-    navButton.classList.toggle("show");
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('show');
+    navLink.classList.toggle('show');
 });
