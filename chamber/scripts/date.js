19ff798e9c7263a2d38195f5595b20bd856cac3f
diegothomas2025 <diegothomas4@hotmail.com>
2026-03-14 19:38:33 -0300

    W02 Assignment: Chamber Directory Page

---
+++ chamber/scripts/date.js
@@ -0,0 +1,4 @@
+const today = new Date();
+
+document.querySelector('#currentyear').textContent = `©${today.getFullYear()} 🐬 Thomas Gomez 🐬 WDD231`;
+document.querySelector('#lastModified').textContent = `Last modified: ${document.lastModified}`;
