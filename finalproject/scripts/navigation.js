61865cd128a252eb4715ee1c6adea72dbfc36297
¨diegothomas2025¨ <diegothomas4@hotmail.com>
2026-04-06 13:45:51 -0300

    header and footer completed

--- finalproject/scripts/navigation.js
+++ finalproject/scripts/navigation.js
@@ -1,5 +1,7 @@
 const hamButton = document.querySelector('#ham-button');
+const navButton = document.querySelector('#nav-button');
 
 hamButton.addEventListener('click', () => {
     hamButton.classList.toggle('show');
+    navButton.classList.toggle('show');
 });
