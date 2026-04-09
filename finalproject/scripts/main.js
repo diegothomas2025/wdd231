4a707ca9edf9214863813db59ec9f33b9bcde9a6
diegothomas2025 <diegothomas4@hotmail.com>
2026-04-08 23:26:50 -0300

    jobs.js  added

--- finalproject/scripts/main.js
+++ finalproject/scripts/main.js
@@ -14,53 +14,6 @@ getTestimonialData()
 import { getServiceData } from './services.js';
 getServiceData()
 
-const url = "https://diegothomas2025.github.io/wdd231/finalproject/data/jobs.json";
-const jobsContainer = document.querySelector('#jobs-container');
-
-async function getJobsData() {
-    try {
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json();
-            console.log(data)
-            displayJobs(data);
-        } else {
-            throw Error (await response.text())
-        } 
-    } catch (error) {
-        console.log(error)
-    }   
-};
+// DISPLAY JOBS
+import { getJobsData } from './jobs.js';
 getJobsData()
-
-function displayJobs(data) {
-
-    data.forEach(element => {
-
-        // CREATE ELEMENTS
-        const div = document.createElement('div');
-        const title = document.createElement('h3');
-        const image = document.createElement('img');
-        const car = document.createElement('p');
-        const duration = document.createElement('p');
-        const location = document.createElement('p');
-        
-        // BUILD CARDS
-        title.textContent = element.service;
-        car.textContent = `Car: ${element.car}`;
-        duration.textContent = `Duration: ${element.duration}`;
-        location.textContent = `Location: ${element.location}`;
-
-        // SET ATTRIBUTES
-        image.setAttribute('src', element.image);
-        image.setAttribute('alt', `${element.car} photo`);
-
-        //APPEND CHILD ELEMENTS AND BUILD CARD STRUCTURES
-        div.appendChild(title);
-        div.appendChild(image);
-        div.appendChild(car);
-        div.appendChild(duration);
-        div.appendChild(location);
-        jobsContainer.appendChild(div);
-    });
-}
